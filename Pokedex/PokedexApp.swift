e4851db7129668351c51a5e48d0f87aa63edbe58
Semih Cetin <semihcettin@gmail.com>
2022-10-16 20:48:24 +0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Semih Cetin on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
